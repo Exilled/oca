2ace46b9d584e656bc6a401451b98e3bad283b48
Exilled <andre.brites.11@gmail.com>
2016-01-14 21:07:04 +0000

    Second commit

--- ShoppingCart/src/ShoppingCart.java
+++ ShoppingCart/src/ShoppingCart.java
@@ -15,29 +15,44 @@ public class ShoppingCart {
 	public void addProduct(Product product){
 		
 	
-	
-	for(int x = 0;x<products.length;x++){
-		
-		if(products[x] == null){
-			products[x] = product;
-			total = total + product.price;
-			break;
+		if(!finished && cartSize < products.length){
+			
+			products[cartSize++] = product;
+			total += product.price;
+			
 		}
+	
+		//for(int x = 0;x<products.length;x++){
+		
+			//if(products[x] == null){
+				//products[x] = product;
+				//total = total + product.price;
+				//break;
+		//}
 		
 		
 		//if(products[x] != null){
 		//	continue;
 		//}
 		//products[x] = product;
-		
-	}
-		
+				
 		
 	}
 	
 	public void removeProduct(int id){
 		
-		//TODO removeProduct
+		if(finished) return;
+		
+		for (int i = 0; i < products.length; i++) {
+			
+			if(products[i].id == id){
+				total -= products[i].price;
+				products[i] = products[--cartSize];
+				products[cartSize] = null;
+				break;
+			}
+			
+		}
 		
 	}
 	
@@ -47,5 +62,23 @@ public class ShoppingCart {
 		
 	}
 	
+	public void finished(){
+		finished = true;
+	}
+	
+	public void printProduct(){
+		
+		for (int i = 0; i < products.length; i++) {
+			
+			if(products[i] != null){
+				System.out.println("---------\nPRODUTO:"+ i);
+				System.out.println(products[i].id);
+				System.out.println(products[i].name);
+				
+			}
+			
+		}
+		
+	}
 	
 }
---
+++ ShoppingCart/src/Store.java
@@ -0,0 +1,60 @@
+
+public class Store {
+
+	public static void main(String[] args) {
+		
+		
+		Customer customer = new Customer();
+		customer.name = "Diogo";
+		
+		Product p1 = new Product();
+		p1.id= 1;
+		p1.name = "batatas";
+		p1.price = 2.3f;
+		
+		Product p2 = new Product();
+		p2.id= 2;
+		p2.name = "couves";
+		p2.price = 0.6f;
+		
+		Product p3 = new Product();
+		p3.id= 3;
+		p3.name = "cenouras";
+		p3.price = 1.4f;
+		
+		
+		
+		ShoppingCart shoppingCart = new ShoppingCart();
+		
+		
+		shoppingCart.customer = customer;
+		
+		shoppingCart.products = new Product[10];
+		
+		shoppingCart.addProduct(p1);
+		shoppingCart.addProduct(p2);
+		shoppingCart.addProduct(p3);
+		
+		System.out.println("Total: " + shoppingCart.total);
+		System.out.println("Customer Name: "+ customer.name);
+		System.out.println("SC Customer Name: "+ shoppingCart.customer.name);
+		
+		System.out.println("OLD CART");
+		shoppingCart.printProduct();
+		
+		shoppingCart.removeProduct(p2.id);
+		
+		System.out.println("\nNEW CART");
+		shoppingCart.printProduct();
+		
+		
+		System.out.println("[B] Finished: "+ shoppingCart.isFinished());
+		shoppingCart.finished();
+		System.out.println("[A] Finished: "+ shoppingCart.isFinished());
+	
+		shoppingCart.addProduct(p1);
+		shoppingCart.printProduct();
+	
+	}
+
+}
